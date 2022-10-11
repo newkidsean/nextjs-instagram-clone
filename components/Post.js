e5d11d5dd5da9c9e5fe47111c1334e4827fc16b7
newkidsean <sean1.onuii@gmail.com>
2022-10-11 22:53:50 +0900

    feat: get comments data and show them on the screen

--- components/Post.js
+++ components/Post.js
@@ -1,13 +1,22 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { DotsHorizontalIcon, HeartIcon, ChatIcon, BookmarkIcon, EmojiHappyIcon } from '@heroicons/react/outline';
 import { userState } from '../atom/userAtom';
 import { useRecoilState } from "recoil";
-import { addDoc, collection, serverTimestamp } from 'firebase/firestore';
+import { addDoc, collection, onSnapshot, orderBy, query, serverTimestamp } from 'firebase/firestore';
 import { db } from '../firebase';
+import Moment from 'react-moment';
 
 const Post = ({ username, img, userImg, caption, id }) => {
   const [currentUser, setCurrentUser] = useRecoilState(userState);
   const [comment, setComment] = useState('');
+  const [comments, setComments] = useState([]);
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(
+      query(collection(db, 'posts', id, 'comments'), orderBy('timestamp', 'desc')), (snapShot) => setComments(snapShot.docs)
+    );
+
+  }, [db, id])
 
   const sendComment = async (event) => {
     event.preventDefault();
@@ -52,6 +61,20 @@ const Post = ({ username, img, userImg, caption, id }) => {
       <p className='p-5 truncate'>
         <span className='font-bold mr-2'>{username}</span>{caption}
       </p>
+      {comments.length > 0 && (
+        <div className='mx-10 max-h-24 overflow-y-scroll scrollbar-none'>
+          {comments.map(comment => (
+            <div key={comment.data().timestamp} className='flex items-center space-x-2 mb-2'>
+              <img
+                className='h-7 rounded-full object-cover'
+                src={comment.data().userImage} alt='user-image' />
+              <p className='font-semibold'>{comment.data().username}</p>
+              <p className='flex-1 truncate'>{comment.data().comment}</p>
+              <Moment fromNow >{comment.data().timestamp?.toDate()}</Moment>
+            </div>
+          ))}
+        </div>
+      )}
       {/* Post input box */}
       {currentUser && (
         <form className='flex items-center p-4'>
